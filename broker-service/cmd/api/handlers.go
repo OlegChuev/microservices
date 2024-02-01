3cb48363ffdc776f0c20a6185e17836178fa63de
OlegChuev <o.chuev@mobidev.biz>
2024-02-01 08:25:02 +0200

    Base broker service

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := JsonResponse{
+		Error:   false,
+		Message: "Hit the Broker",
+	}
+
+	_ = app.WriteJson(w, http.StatusOK, payload)
+}
